bf26fc204b169b1a22a4173e500299ff7dd617c8
kylee9612 <iiii410@naver.com>
2022-06-20 12:48:06 +0900

    comment added

--- programmer/src/programmers/FindNumber.java
+++ programmer/src/programmers/FindNumber.java
@@ -1,5 +1,13 @@
 package programmers;
 
+/*
+ * https://programmers.co.kr/learn/courses/30/lessons/86051?language=java
+ * 
+ * 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. 
+ * numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+ *
+ */
+
 class Find{
 	public int solution(int[] numbers) {
         int answer = 0;
