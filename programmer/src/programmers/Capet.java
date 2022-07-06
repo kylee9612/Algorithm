57cea598f0b0737d0824187df5cd141bd1da93da
kylee9612 <iiii410@naver.com>
2022-07-06 09:28:41 +0900

    1

---
+++ programmer/src/programmers/Capet.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class Capet {
+	public int[] solution(int brown, int yellow) {
+		int[] answer = new int [2];
+		
+		int size = brown + yellow;
+		int maxX = 0;
+		int minY = 0;
+		for(int i = 0 ; i < size; i++) {
+			
+		}
+		return answer;
+	}
+}
